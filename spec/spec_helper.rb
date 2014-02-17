521bb46a156936a83f808f8fb8549fb817468ec4
Sebastian Sucker <sebastian@endocode.com>
2014-02-17 17:02:13 +0100

    Removes filter for endobot.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,11 @@ require 'simplecov'
 SimpleCov.start do
   add_filter '/spec/'
   add_filter '/vendor/'
-  add_filter 'endobot.rb'
+  #add_filter 'endobot.rb'
 end
 
 require 'faker'
 require_relative '../report'
 require_relative '../endobot'
 
-SimpleCov.minimum_coverage 90
+#SimpleCov.minimum_coverage 90
